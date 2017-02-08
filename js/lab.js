98649b2f02f747e1c51c018a302986acf2102722
Craig Morey <craigmorey@gmail.com>
2017-02-08 09:24:56 +0100

    Updates

--- js/lab.js
+++ js/lab.js
@@ -14695,6 +14695,7 @@ function getMonday(d) {
   });
   
 })();
+/*jshint esversion: 6 */
 // test to see if local/session storage is enabled in the current client
 var testForLocalStorage = function() {
     try {
@@ -14727,6 +14728,38 @@ var storageRemove = function(key) {
     localStorage.removeItem(key + 'DateStamp');
     return localStorage.removeItem(key);
 };
+
+
+
+self.addEventListener('install', event => {
+  console.log('V1 installing…');
+
+  // cache a cat SVG
+  event.waitUntil(
+    caches.open('static-v1').then(cache => cache.add('/cat.svg'))
+  );
+});
+
+self.addEventListener('activate', event => {
+  console.log('V1 now ready to handle fetches!');
+});
+
+self.addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+
+  // serve the cat SVG from the cache if the request is
+  // same-origin and the path is '/dog.svg'
+  if (url.origin == location.origin && url.pathname == '/dog.svg') {
+    event.respondWith(caches.match('/cat.svg'));
+  }
+});
+
+
+
+
+
+
+
 /*
     Fonts are loaded through @font-face rules in the CSS whenever an element references them.
     FontFaceObserver creates a referencing element to trigger the font request, and lsisten for font load events.
@@ -14912,6 +14945,15 @@ var gaConfig = {
       'colors': {
         1: '#fff',
         2: '#5CA1C4'
+      }
+    },
+    {
+      'name': 'ST',
+      'abbr': 'st',
+      'view': '2783821',
+      'colors': {
+        1: '#fff',
+        2: '#c26625'
       }
     },
     {
